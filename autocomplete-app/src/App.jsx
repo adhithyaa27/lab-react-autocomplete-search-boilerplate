b9f6245c77a91ef38f80a695322eb00136f3abd0
Shaswath <shaswath.g@kalvium.community>
2024-01-22 17:56:18 +0530

    done

---
+++ autocomplete-app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Autocomplete from './Components/Autocomplete'
+
+function App() {
+  return (
+    <>
+      <Autocomplete/>
+    </>
+  )
+}
+
+export default App
